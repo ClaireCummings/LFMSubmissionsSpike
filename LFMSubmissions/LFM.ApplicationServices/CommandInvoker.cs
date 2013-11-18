844ee3d77d78fd8d19765b3801fc7de8c0f60fd7
AndrewKindleysides <andrew.kindleysides@advancedcomputersoftware.com>
2013-11-18 17:52:52 +0000

    Added CommandInvoker

---
+++ LFMSubmissions/LFM.ApplicationServices/CommandInvoker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LFM.ApplicationServices.LandRegistry;
+
+namespace LFM.ApplicationServices
+{
+    public class CommandInvoker : ICommandInvoker
+    {
+        private readonly SubmissionDataService _submissionDataService;
+
+        public void Execute<TCommand>(TCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResponse Execute<TCommand, TResponse>(TCommand command)
+        {
+            return ((IHandleCommand<TCommand, TResponse>) _submissionDataService).Execute(command);
+        }
+    }
+}
---
+++ LFMSubmissions/LFM.ApplicationServices/ICommandInvoker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFM.ApplicationServices
+{
+    public interface ICommandInvoker
+    {
+        void Execute<TCommand>(TCommand command);
+        TResponse Execute<TCommand, TResponse>(TCommand command);
+    }
+}
